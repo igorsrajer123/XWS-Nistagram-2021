6361b1ad9dda4b4ad7bf6d58eaddcb2a975dda78
igorsrajer123 <igorsrajer123@gmail.com>
2021-05-16 13:51:32 +0200

    Initial project upload

---
+++ github.com/api/user-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+}
